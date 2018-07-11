8848b80e0c7b7bedcb74dbb4828512ffc8e4b09b
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-11 07:55:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function sayHiToGrandma(string){
    return "I can\'t hear you!"
  }
  else if(string == toUpperCase){
-   
+   return "YES INDEED"
 }
 else if(string == "I love you, Grandma." ){
-  return ""
+  return "I love you, too."
 }
 }
